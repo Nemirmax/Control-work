2f986710f1303d5b28728b151245abffd8b19939
Maxim <maxim.nemer@mail.ru>
2022-06-16 14:51:31 +0300

    код написан,итоговый коммит

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿//Написать программу, которая из имеющегося массива строк формирует массив 
 
-string[] array = { "кря", "привет", "123", ":-)" };
+string[] array = { "массив", "привет", "123", ":-)" };
+
+string[] newarray = new string[0];
+Console.Write("[");
+for (int i = 0; i < array.Length; i++)
+{
+    int j = 0;
+    if (array[i].Length < 4)
+    { 
+        Array.Resize(ref newarray, j + 1);
+        newarray[j] = array[i];
+        Console.Write($"{newarray[j]}; ");
+    }
+}
+Console.Write("]");
+Console.WriteLine(); 
